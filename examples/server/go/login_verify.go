9840f48eb53749f758c2a53acfbfce3e9486c019
gaohongming <gaohongming@ztgame.com>
2018-01-23 10:52:18 +0800

    拼接字符串方法修改

--- examples/server/go/login_verify.go
+++ examples/server/go/login_verify.go
@@ -9,31 +9,39 @@ import (
 	"encoding/json"
 	"encoding/pem"
 	"fmt"
-	URL "net/url"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 var (
 	//公钥
 	pubPem = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA/VweA5KME/PR7QIwe+Bo
-Wf+yM5tRVpaXWOZC7S4SeLT5zyd1gNzKjLHCxAIhsxYUnXHRCdsC+cnKTVfCBuew
-v7N2kZCKF+/gMiqSdfiJo3XE7lmrxpIKO6YnWt7Itq/VvMJoTO7g0KkjF/irzI+O
-KUj5DSZORHwulA6OXNjUJbGeMdIGX7VHgbk7dv8oqx+FE0bAQ4APtOiSs5agBRdg
-9De92tURcj15jztYtPvaRVn5O8ozTEui4Kh2Cmf1fPFKbv5yQyNhHaqMdI2tGPRE
-s78wGIiHT1yOmCMeKHCVgElwwgmFnmxKYsBD9XZ9GM6wzt/95M53jh/aNp/+9Y1m
-0QIDAQAB
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2aVhd+cDlvdel8x2jWL1
+Gct39umu5xxeQ2M6HheiMkawbO7klT/2Nc2Ro3Xq6qp69Q7zwCeFo8egQDiZsCPu
+04RdasgwKPNQN4vsUPvpk1OTO+pa8zzx/YwhdYChrjeKpSxMoFJujzO6qh+Pc81A
+nMOgVwISXF3WuM49Bkob+vvTOynApBaYBZndBmomVWTezQIU8X2+TmgvQaqxLc/6
+lQW+OVp+gUcILg0fVebTpE1ZghMVrihq0/DvQ4IOYC18gINzNBqxnSv0ggl59A60
+7nYFZZYOMh05m7bgjOOCGIVTKTurM7++VqD4suptBXWU1G/PGN/26CgFumj28oGZ
+wQIDAQAB
 -----END PUBLIC KEY-----`
 	//登录用户信息
-	params = `{"entity":{"openid":"1-123123","account":"test","time":1482313093},"sign":"m7JTn\/y3IpB084vyeqoR9ysZ5\/GeowcsO3KDrDsaa8Fof2Xjq4gKdk5eDLh0nTRxZslfbet5AWs+p1M0rtF8Jan8T2VxCW\/czoCkrj4o\/xnYtb3wdenCXAT7LUoydjTmd+cf0I9kw0DjkPDurrT9kNqxzw6dq1A6EFZAts4f0\/H5+7kn81rzPq1RkWOM6OGm8R2D2WW\/jHfqZDo1mvfZvIzkA\/F0M62z2VsUK821BUkgoT23dByCdeqgx4hauJTHlnvg2\/MplDYMDONHai6gTFWb4FQgY2wJq1BIWCyXgDuid6n9Ck0m6paJEfOffbK7BgwH3ssaF7xYGD2sjEuCFw=="}`
+	params = `{"entity":{"openid":"1-2330056908","account":"_80_m_tfghm@163.com","account_type":24,"type":1,"time":1516673192},"sign":"CGFsUW+TLKoQOxZdbY0OlptZN4OcM0vlAz6ocfuSO/Op8MO728miG+eN/+1AeMkn53w3Fo1j3hdFspDhUUN1FDrsRSnjENmHGDus9zc4H3nkxL+Viv8rHQRsbsDPOF5Qfj7tLGAzt9CvNSh6bJfP02PZrA1VzUlaA9M68EtqUXNKzyLfw0bnW1MVPdLfY9edtmo/9WwbwXlgUsOuOB4LYHQd0H84JSfq1ify7Y9KPi1U+Z8caleygKf8rp8pQXP36a5lMXNyU9Md97yCVZurhzBihlM33O/A+SLz/1piFASV2TujtQ2ltWLOSDCU+NnX34SnNPchBxYV366BUh5RmQ=="}`
 	//json解析对像
 	jsonParams interface{}
 )
 
 //拼接验签字符串，并key正序排序，格式如：key1=value1&key2=value2&key3=value3
 func HttpBuildQuery(params map[string]interface{}) string {
-	c := URL.Values{}
-	for k, v := range params {
+	var keys []string
+	for k := range params {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	var query []string
+	for _, k := range keys {
+		v := params[k]
 		str := ""
 		switch v.(type) {
 		case int:
@@ -44,9 +52,10 @@ func HttpBuildQuery(params map[string]interface{}) string {
 		case float64:
 			str = strconv.Itoa(int(v.(float64)))
 		}
-		c.Add(k, str)
+		query = append(query, fmt.Sprintf("%s=%s", k, str))
 	}
-	return c.Encode()
+	return strings.Join(query, "&")
+
 }
 
 func main() {
@@ -65,7 +74,7 @@ func main() {
 
 	entity := data["entity"].(map[string]interface{})
 	query := HttpBuildQuery(entity)
-	//fmt.Println(query, data["sign"].(string))
+	fmt.Println(query, data["sign"].(string))
 
 	//验签
 	block, _ := pem.Decode([]byte(pubPem))
